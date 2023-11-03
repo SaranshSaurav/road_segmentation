7724ed6f3dda2d839f99b7b0f03b71c726cedf36
SaranshSaurav <93869999+SaranshSaurav@users.noreply.github.com>
2023-11-03 17:36:21 +0530

    Add files via upload

---
+++ utils/config.py
@@ -0,0 +1,15 @@
+# use GPU for training
+USE_GPU = 1
+
+# image strored
+IMG_W,IMG_H = (160,120)
+NET_SHAPE=(160,20)
+IMG_CHANNELS = 3
+
+# trained model
+MODEL_FILE = '.\\seg.h5'
+
+# training /test database releated info
+DATA_LOCATION='.\\data\\**\\'
+FILE = "*info.rec"
+
